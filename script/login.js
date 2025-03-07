d23ed6e7c4769f5f364344fb34c4e8eba2b11062
RubayetHasan -Git <dev.rubayethasan@gmail.com>
2025-03-07 08:14:52 -0800

    added main html file and also added some header section

--- script/login.js
+++ script/login.js
@@ -1,5 +1,20 @@
 document.getElementById('login-btn')
-.addEventListener('click',function(){
-    console.log("hello")
-
+.addEventListener('click',function(event){
+    event.preventDefault();
+    const acountNumber = document.getElementById('Acount-number').value;
+    // console.log(acountNumber)
+    const acountPin  = document.getElementById('Acount-pin').value;
+    const convertPin = parseInt(acountPin);
+    // console.log(acountPin);
+    if(acountNumber.length === 11 && acountPin.length === 4){
+        if(convertPin === 1234   ){
+            window.location.href= "./main.html"
+        }
+        else{
+            alert("pin thik nai"); 
+        }
+    }
+    else{
+        alert("need a valid account number");
+    }
 })
